bcad0c0b7ecd9ea21a05c3d4e4a1a93e7fb3be44
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2024-12-09 16:54:28 +0300

    Update books.js

--- src/components/books.js
+++ src/components/books.js
@@ -5,21 +5,35 @@ const Books = () => {
         <div>
             <div className="d-flex justify-content-evenly">
                 <section id="feature" className="book-feature">
-
                     <div className="row">
-                        {Data.map((element) => (
-                            <div className="book-item">
-                                <div className="book feature-box col-md-4">
-                                    <div className="align"> <img src={element.img} alt="" />
-                                        <h4>{element.title}</h4></div>
-                                    <p>{element.body}</p>
+                        {Data.map((element, index) => (
+                            <div key={index} className="col-md-4 mb-4 d-flex justify-content-center">
+                                <div className="book-item card shadow border-0" style={{ width: "18rem" }}>
+                                    <div className="book feature-box card-body text-center">
+                                        <div className="align mb-3">
+                                            <img
+                                                src={element.img}
+                                                alt={element.title}
+                                                className="img-fluid rounded"
+                                                style={{ maxHeight: "150px", objectFit: "cover" }}
+                                            />
+                                        </div>
+                                        <h4 className="card-title text-primary">{element.title}</h4>
+                                        <p className="card-text text-muted">{element.body}</p>
+                                        <Link to='/'>
+                                            <button type="button" className="btn btn-light mb-2">
+                                                Download
+                                                <span className="badge bg-white text-dark ms-2">
+                                                    <i className="bi bi-cloud-arrow-down-fill"></i>
+                                                </span>
+                                            </button>
+                                        </Link>
+                                    </div>
                                 </div>
-                                <Link to='/'> <button type="button" class="btn btn-light mb-2">
-                                    Download <span class="badge bg-white text-dark"><i className="bi bi-cloud-arrow-down-fill"></i></span>
-                                </button></Link>
                             </div>
                         ))}
                     </div>
+
                 </section >
             </div>
         </div>
