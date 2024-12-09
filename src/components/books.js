0597852c000acebfce2419f332f2253537277196
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2024-12-09 17:01:06 +0300

    Update books.js

--- src/components/books.js
+++ src/components/books.js
@@ -1,41 +1,37 @@
 import { Link } from "react-router-dom";
 import Data from "./data";
+
 const Books = () => {
     return (
-        <div>
-            <div className="d-flex justify-content-evenly">
-                <section id="feature" className="book-feature">
-                    <div className="row">
-                        {Data.map((element, index) => (
-                            <div key={index} className="col-md-4 mb-4 d-flex justify-content-center">
-                                <div className="book-item card shadow border-0" style={{ width: "18rem" }}>
-                                    <div className="book feature-box card-body text-center">
-                                        <div className="align mb-3">
-                                            <img
-                                                src={element.img}
-                                                alt={element.title}
-                                                className="img-fluid rounded"
-                                                style={{ maxHeight: "150px", objectFit: "cover" }}
-                                            />
-                                        </div>
-                                        <h4 className="card-title text-primary">{element.title}</h4>
-                                        <p className="card-text text-muted">{element.body}</p>
-                                        <Link to='/'>
-                                            <button type="button" className="btn btn-light mb-2">
-                                                Download
-                                                <span className="badge bg-white text-dark ms-2">
-                                                    <i className="bi bi-cloud-arrow-down-fill"></i>
-                                                </span>
-                                            </button>
-                                        </Link>
-                                    </div>
+        <div className="container py-5">
+            <section id="feature" className="book-feature">
+                <div className="row g-4">
+                    {Data.map((element, index) => (
+                        <div key={index} className="col-md-4">
+                            <div className="card shadow-sm h-100 border-0">
+                                <img
+                                    src={element.img}
+                                    alt={element.title}
+                                    className="card-img-top rounded-top"
+                                    style={{ maxHeight: "200px", objectFit: "cover" }}
+                                />
+                                <div className="card-body text-center">
+                                    <h5 className="card-title text-primary">{element.title}</h5>
+                                    <p className="card-text text-muted">{element.body}</p>
+                                    <Link to="/">
+                                        <button type="button" className="btn btn-outline-primary">
+                                            Download
+                                            <span className="ms-2">
+                                                <i className="bi bi-cloud-arrow-down-fill"></i>
+                                            </span>
+                                        </button>
+                                    </Link>
                                 </div>
                             </div>
-                        ))}
-                    </div>
-
-                </section >
-            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
         </div>
     );
 }
